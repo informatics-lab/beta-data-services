2f071fce8647f6d6f4f2374ad92a9976e4560b8b
Simon Stanley <simon.stanley87@gmail.com>
2015-04-23 12:12:15 +0100

    Added assertions to parameter dict method

--- BDS/requester.py
+++ BDS/requester.py
@@ -157,18 +157,57 @@ class BDSRequest(object):
                               " [x-min, y-min, x-max, y-max]")
 
     @staticmethod
+    def _check_dim_forecast(dim_fcst):
+        """
+        Check the dim_forecast is valid format.
+
+        """
+        if type(dim_fcst) != str:
+            raise ValueError("dim_forecast must be given as a string.")
+
+    @staticmethod
+    def _check_grid_num(grid_num):
+        """
+        Check number grid points is valid format.
+
+        """
+        try:
+            int(grid_num)
+        except ValueError:
+            raise ValueError("Width/height values must be integer like.")
+
+    @staticmethod
+    def _check_grid_size(grid_size):
+        """
+        Check number grid points is valid format.
+
+        """
+        try:
+            float(grid_size)
+        except ValueError:
+            raise ValueError("resx/resy values must be float like.")
+
+    def _check_interpolation(interpolation):
+        """
+        Check valid interpolation format
+
+        """
+        if type(interpolation) != str:
+            raise ValueError("interpolation must be given as a string.")
+
+    @staticmethod
     def _sort_time(time):
         """
-        Check time string is valid and return in ISO format.
+        Check time string is valid time format and return in ISO format.
 
         """
         try:
             time_str = dateutil.parser.parse(time).isoformat()
-            if time_str[-1] != "Z":
-                time_str += "Z"
-            return time_str
-        except:
-            raise UserWarning("Invalid time argument given: %s" % time)
+        except ValueError, AttributeError:
+            raise ValueError("Invalid time argument given: %s" % time)
+        if time_str[-1] != "Z":
+            time_str += "Z"
+        return time_str
 
     @staticmethod
     def _sort_bbox(bbox):
@@ -382,25 +421,29 @@ class BDSRequest(object):
 
         return cubes
 
-    def getParameterDictionary(self, format=None, crs=None, elevation=None, bbox=None, dim_run=None, time=None, dim_forecast=None,  width=None, height=None, resx=None, resy=None, resz=None, interpolation=None):
+    def getParameterDictionary(self, format, crs, elevation, bbox=None,
+                               dim_run=None, time=None, dim_forecast=None,
+                               width=None, height=None, resx=None, resy=None,
+                               interpolation=None):
         """
-        Create a dictionary of valid parameters for getCoverage method.
+        Create a dictionary of valid parameters for getCoverage method. The
+        parameters format, crs and elevation must be specified for a valid
+        request. Use describeCoverage to print out avaiable options for these.
 
-        Kwargs:
+        Notes:
 
-        * bbox: list
-            Must contain 4 values in the format [x-min, y-min, x-max, y-max].
-            Values can be given as integers, floats or strings.
+        When specifing times; dim_run, time and dim_forecast cannot all be
+        given (even if they fit). Only a maximum of two can be specified.
 
-        * dim_run: string (ISO format)
-            The model run time.
+        When specifying resolutions, width and height specify how many grid
+        boxes are returned, the size of the boxes is consequential. resx and
+        resy specify the size of the grid boxes and the number of grid boxes
+        is consequential. They can not be used together (even if the fit).
 
-        * time: string (ISO format)
-            The forecast time
+        This method does not verify if the given parameters are available, it
+        only asserts they are in the correct format.
 
-        * dim_forecast: string
-            The time releative to the dim_run e.g. PT36H is 36 hours from the
-            model run time.
+        Args:
 
         * format: string
             The format for the data to be returned in, e.g. NetCDF3
@@ -408,17 +451,38 @@ class BDSRequest(object):
         * crs: string
             The coordinate reference system, e.g. EPSG:4326
 
+        * elevation: string
+            The veritcal level description.
+
+        Kwargs:
+
+        * bbox: list
+            Must contain 4 values in the format [x-min, y-min, x-max, y-max].
+            Values can be given as integers, floats or strings. Default is the
+            entire field is returned.
+
+        * dim_run: string (date string)
+            The model run time. This can be specified using most convential
+            date descriptions e.g. 1/2/2015 or 2015-02-01 or 1st January 2015.
+            Default is the latest run (unless time AND dim_forecast are given).
+
+        * time: string (date string, see dim_run)
+            The forecast time. Default is the first forecast time from the
+            dim_run (unless dim_run AND dim_forecast are given).
+
+        * dim_forecast: string
+            The time releative to the dim_run e.g. PT36H is 36 hours from the
+            model run time. Default is PT0H (unless dim_run AND time are
+            given). It is valid to provide just the number.
+
         * width/height: integer
             The number of gridpoints in the x (width) and y (height) within
             the bounding box. Interpolation is used.
 
-        * resx/resy/resz: float
-            The size of a gridpoint in the x/y/z direction. This is an
+        * resx/resy: float
+            The size of a gridpoint in the x/y direction. This is an
             alternative method to specifing the width/height. Interpolation is
             used.
-
-        * elevation: string
-            The veritcal level.
 
         * interpolation: string
             The interpolation method used if data is re-gridded.
@@ -428,10 +492,21 @@ class BDSRequest(object):
 
         """
         param_dict = {}
+
+        # Add essential parameters.
+        param_dict["FORMAT"]    = format
+        param_dict["CRS"]       = crs
+        param_dict["ELEVATION"] = elevation
+
         if bbox:
             self._check_bbox(bbox)
             bbox_str = self._sort_bbox(bbox)
             param_dict["BBOX"] = bbox_str
+
+        # Time parameters.
+        if dim_run and time and dim_forecast:
+            raise UserWarning("Cannot use more than 2 of dim_run, "\
+                              "dim_forecast or time parameters together.")
         if dim_run:
             dim_run = self._sort_time(dim_run)
             param_dict["DIM_RUN"] = dim_run
@@ -439,23 +514,31 @@ class BDSRequest(object):
             time = self._sort_time(time)
             param_dict["TIME"] = time
         if dim_forecast:
+            self._check_dim_forecast(dim_forecast)
             param_dict["DIM_FORECAST"] = dim_forecast
-        if format:
-            param_dict["FORMAT"] = format
-        if crs:
-            param_dict["CRS"] = crs
+
+        # Resolution parameters.
+        if width and resx:
+            raise UserWarning("Cannot specify width and resx together; one "\
+                              "implies the other.")
         if width:
+            self._check_grid_num(width)
             param_dict["WIDTH"] = width
-        if height:
-            param_dict["HEIGHT"] = height
         if resx:
+            self._check_grid_size(resx)
             param_dict["RESX"] = resx
+
+        if height and resy:
+            raise UserWarning("Cannot specify height and resy together; one "\
+                              "implies the other.")
+        if height:
+            self._check_grid_num(height)
+            param_dict["HEIGHT"] = height
         if resy:
+            self._check_grid_size(resy)
             param_dict["RESY"] = resy
-        if resz:
-            param_dict["RESZ"] = resz
-        if elevation:
-            param_dict["ELEVATION"] = elevation
+
         if interpolation:
+            self._check_interpolation(interpolation)
             param_dict["INTERPOLATION"] = interpolation
         return param_dict
